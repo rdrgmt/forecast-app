93006f8d21d3ab9e2a75f2734fa6657fe2c96a0e
matheus <rdrgmt@proton.me>
2024-05-24 12:45:47 -0300

    CHORE: init config files

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import './style.css';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+const view = new View({
+    center: [0, 0],
+    zoom: 2,
+});
+
+const layer = new TileLayer({
+    source: new OSM(),
+});
+
+const map = new Map({
+    target: 'map',
+    layers: [layer],
+    view: view,
+});
